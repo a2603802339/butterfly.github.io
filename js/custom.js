cb04fc51ffd9597349b59fcf4a5a3adffdadcfd3
a2603802339 <a2603802339@gamil.com>
2022-03-29 17:20:02 +0800

    Site updated: 2022-03-29 17:20:01

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,1 @@
-/*去掉 Banner 图*/
-var full_page = document.getElementsByClassName("full_page");
-if (full_page.length != 0) {
-  full_page[0].style.background = "transparent";
-}
-
+var full_page=document.getElementsByClassName("full_page");0!=full_page.length&&(full_page[0].style.background="transparent");
